aac6bcce616842c8e701112f3864f3a67397ed79
rodrigo-pino <rodrodpino@gmail.com>
2022-03-15 00:40:18 -0400

    Add constructor to Gear contracts and other minor changes

--- backend/contracts/NFT/DefensiveGear.sol
+++ backend/contracts/NFT/DefensiveGear.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./Gear.sol";
 
-contract DefensiveGear is Gear("Defensive Gear", "DGR") {}
+contract DefensiveGear is Gear {
+     constructor (uint[] memory _amount, uint[] memory _price, string[] memory _uris)
+     Gear(_amount, _price, _uris, "Defensive Gear", "DFGR") {}
+}
--- backend/contracts/NFT/OffensiveGear.sol
+++ backend/contracts/NFT/OffensiveGear.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./Gear.sol";
 
-contract OffensiveGear is Gear("Offensive Gear", "OGR") {}
+contract OffensiveGear is Gear {
+     constructor (uint[] memory _amount, uint[] memory _price, string[] memory _uris)
+     Gear(_amount, _price, _uris, "OffensiveGear Gear", "OFGR") {}
+}
